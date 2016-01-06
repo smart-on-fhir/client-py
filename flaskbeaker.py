e7348e148799f0168e58571f97acca3b47c8d42c
Devin Fisher <dev.fisher@gmail.com>
2016-01-06 16:43:31 -0700

    changed tabs to 4 spaces to be pep8 compliant.

--- flaskbeaker.py
+++ flaskbeaker.py
@@ -6,26 +6,26 @@ from beaker.middleware import SessionMiddleware
 
 
 class FlaskBeaker(SessionInterface):
-	""" Beaker session interface class for Beaker sessions in Flask apps.
-	Stolen from http://flask.pocoo.org/snippets/121/ (public domain)
-	"""
-	@classmethod
-	def setup_app(cls, app, session_opts=None):
-		if session_opts is None:
-			session_opts = {
-				'session.type': 'file',
-				'session.timeout': 3600,
-				'session.cookie_expires': 3600,
-				'session.data_dir': './session_data',
-				'session.auto': True
-			}
-		
-		app.wsgi_app = SessionMiddleware(app.wsgi_app, session_opts)
-		app.session_interface = cls()
-	
-	def open_session(self, app, request):
-		session = request.environ['beaker.session']
-		return session
-	
-	def save_session(self, app, session, response):
-		session.save()
+    """ Beaker session interface class for Beaker sessions in Flask apps.
+    Stolen from http://flask.pocoo.org/snippets/121/ (public domain)
+    """
+    @classmethod
+    def setup_app(cls, app, session_opts=None):
+        if session_opts is None:
+            session_opts = {
+                'session.type': 'file',
+                'session.timeout': 3600,
+                'session.cookie_expires': 3600,
+                'session.data_dir': './session_data',
+                'session.auto': True
+            }
+        
+        app.wsgi_app = SessionMiddleware(app.wsgi_app, session_opts)
+        app.session_interface = cls()
+    
+    def open_session(self, app, request):
+        session = request.environ['beaker.session']
+        return session
+    
+    def save_session(self, app, session, response):
+        session.save()
