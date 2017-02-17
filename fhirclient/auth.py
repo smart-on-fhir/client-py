2b9d2a8bc92fa07d0333648037ce697f502ce0d5
Trinadh Baranika <trinadh@cura.tech>
2017-02-17 10:51:01 +0530

    The refresh token issued by the authorization server. If present, the app should discard any previous refresh_token associated with this launch, replacing it with this new value

--- fhirclient/auth.py
+++ fhirclient/auth.py
@@ -280,11 +280,15 @@ class FHIROAuth2Auth(FHIRAuth):
         if self.access_token is None:
             raise Exception("No access token received")
         del ret_params['access_token']
+
         if 'expires_in' in ret_params:
             del ret_params['expires_in']
-        
-        self.refresh_token = ret_params.get('refresh_token')
-        if self.refresh_token is not None:
+
+        # The refresh token issued by the authorization server. If present, the app should discard
+        # any previous refresh_token associated with this launch, replacing it with this new value.
+        refresh_token = ret_params.get('refresh_token')
+        if refresh_token is not None:
+            self.refresh_token = refresh_token
             del ret_params['refresh_token']
         
         logging.debug("SMART AUTH: Received access token: {0}, refresh token: {1}"
