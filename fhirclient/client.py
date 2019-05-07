91545f1c9eaca7f3b1d4c1fd6e15201274880668
Raheel Sayeed <raheelsayeed@OSXLAP04366.local>
2019-05-07 17:02:44 -0400

    version bump

--- fhirclient/client.py
+++ fhirclient/client.py
@@ -4,7 +4,7 @@ import logging
 
 from server import FHIRServer, FHIRUnauthorizedException, FHIRNotFoundException
 
-__version__ = '3.2.0'
+__version__ = '4.0.0'
 __author__ = 'SMART Platforms Team'
 __license__ = 'APACHE2'
 __copyright__ = "Copyright 2017 Boston Children's Hospital"
