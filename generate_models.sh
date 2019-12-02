863731adf7897d0a460a36127ecb4142ed7ae3ed
Andrew Wason <rectalogic@rectalogic.com>
2019-12-02 10:25:47 -0500

    Only run model unit tests when new models are generated.

--- generate_models.sh
+++ generate_models.sh
@@ -4,6 +4,16 @@ set -o errexit
 
 git submodule update --init --recursive
 
+# set this to the downloaded FHIR spec directory
+export FHIR_UNITTEST_DATADIR="./fhir-parser/downloads"
+
+export SKIP_TESTS=0
+echo 'import isodate' | python 2>/dev/null
+if [ $? -ne 0 ]; then
+	echo You need to have the 'isodate' module installed in order to run the tests
+    SKIP_TESTS=1
+fi
+
 for FHIR_VERSION in R4; do
     FHIR_PARSER=fhir-parser
     sed s/{FHIR_VERSION}/$FHIR_VERSION/g fhir-parser-resources/settings.py > $FHIR_PARSER/settings.py
@@ -11,4 +21,7 @@ for FHIR_VERSION in R4; do
         cd $FHIR_PARSER
         ./generate.py -f $1
     )
+    if [ $SKIP_TESTS -eq 0 ]; then
+        python -m unittest discover -s fhirclient.models.$FHIR_VERSION -p '*_tests.py'
+    fi
 done
--- test_models.sh
+++ test_models.sh
@@ -3,34 +3,12 @@
 export PYTHONPATH=.
 EXITCODE=0
 
-# set this to the downloaded FHIR spec directory
-export FHIR_UNITTEST_DATADIR="./fhir-parser/downloads"
-
-echo 'import isodate' | python 2>/dev/null
-if [ $? -ne 0 ]; then
-	echo You need to have the 'isodate' module installed in order to run the tests
-	exit 1
-fi
-
-for FHIR_VERSION in R4; do
-    # Download into cache for this version
-    rm -rf $FHIR_UNITTEST_DATADIR/*
-    mkdir -p $FHIR_UNITTEST_DATADIR
-    # We can't download because the spec version changes, so copy from cache
-    # e.g. http://hl7.org/fhir/R4 could be 4.0.0 or 4.0.1 etc.
-    # ./generate.py -f -l
-    cp fhir-parser-resources/$FHIR_VERSION/version.info $FHIR_UNITTEST_DATADIR
-    python -m zipfile -e fhir-parser-resources/$FHIR_VERSION/examples-json.zip $FHIR_UNITTEST_DATADIR
-
-    python -m unittest discover -s fhirclient.models.$FHIR_VERSION -p '*_tests.py' || EXITCODE=1
-done
-
 # couple of custom tests
 echo 'import requests' | python 2>/dev/null
 if [ $? -eq 0 ]; then
 	python -m unittest fhirclient.server_tests fhirclient.fhirreference_tests fhirclient.integration_tests || EXITCODE=1
 else
-	echo "You don't have the 'requests' module installed, will skip extra tests"
+	echo "You don't have the 'requests' module installed, will skip tests"
 fi
 
 exit $EXITCODE
