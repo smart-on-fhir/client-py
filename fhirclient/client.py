469f8de02be3054607525c958592689ed22b4fa2
Mark Zbikowski <mark@zbikowski.org>
2022-01-17 10:43:09 -0700

    Adds directory specification for python 3

--- fhirclient/client.py
+++ fhirclient/client.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from server import FHIRServer, FHIRUnauthorizedException, FHIRNotFoundException
+from .server import FHIRServer, FHIRUnauthorizedException, FHIRNotFoundException
 
 __version__ = '4.0.0'
 __author__ = 'SMART Platforms Team'
